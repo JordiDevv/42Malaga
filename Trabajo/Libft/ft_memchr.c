26566299b54a747d455df3819f13b370819d3212
JordiDevv <157953321+JordiDevv@users.noreply.github.com>
2024-04-22 13:31:46 +0200

    Add files via upload

--- Trabajo/Libft/ft_memchr.c
+++ Trabajo/Libft/ft_memchr.c
@@ -1,11 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jsanz-bo <jsanz-bo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/22 10:06:06 by jsanz-bo          #+#    #+#             */
+/*   Updated: 2024/04/22 10:15:23 by jsanz-bo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
-void	*memchr(const void *s, int c, size_t n)
+void	*ft_memchr(const void *s, int c, size_t n)
 {
 	while (n > 0)
 	{
-		if (*(unsigned char)s = (unsigned char)c)
-			return (s);
+		if (*(unsigned char *)s == (unsigned char)c)
+			return ((unsigned char *)s);
+		s++;
 		n--;
 	}
 	return (NULL);
