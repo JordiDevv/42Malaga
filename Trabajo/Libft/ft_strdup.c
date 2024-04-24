bf51f0afd4fc2b7c8e729087d8fad2f74582437f
JordiDevv <157953321+JordiDevv@users.noreply.github.com>
2024-04-24 18:40:50 +0200

    Add files via upload

--- Trabajo/Libft/ft_strdup.c
+++ Trabajo/Libft/ft_strdup.c
@@ -1,26 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jsanz-bo <jsanz-bo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/23 16:11:37 by jsanz-bo          #+#    #+#             */
-/*   Updated: 2024/04/24 10:31:24 by jsanz-bo         ###   ########.fr       */
+/*   Created: 2024/04/24 10:32:56 by jsanz-bo          #+#    #+#             */
+/*   Updated: 2024/04/24 10:57:54 by jsanz-bo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_calloc(size_t count, size_t size)
+char	*ft_strdup(const char *s1)
 {
 	void	*ptr;
-	size_t	t_size;
 
-	t_size = count * size;
-	ptr = malloc(t_size);
-	if (ptr == NULL)
+	ptr = malloc(ft_strlen(s1));
+	if (ptr = NULL)
 		return (NULL);
-	ft_bzero(ptr, t_size);
+	ft_strcpy(ptr, s1);
 	return (ptr);
 }
