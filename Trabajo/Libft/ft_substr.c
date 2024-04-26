0432a2fb2d2e4d1e34a2474d91b69c75e96d97fc
JordiDevv <157953321+JordiDevv@users.noreply.github.com>
2024-04-26 19:32:42 +0200

    Add files via upload

--- Trabajo/Libft/ft_substr.c
+++ Trabajo/Libft/ft_substr.c
@@ -6,27 +6,31 @@
 /*   By: jsanz-bo <jsanz-bo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 16:27:54 by jsanz-bo          #+#    #+#             */
-/*   Updated: 2024/04/25 18:03:44 by jsanz-bo         ###   ########.fr       */
+/*   Updated: 2024/04/26 19:31:09 by jsanz-bo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
-char	*ft_substr(char const *s, unsigned int start, size_t len)
+char	*ft_substr(const char *s, unsigned int start, size_t len)
 {
 	char	*subs;
-	size_t	i;
 
-	i = 0;
+	if (ft_strlen(s) < start)
+		len = 0;
+	if (ft_strlen(s) < len)
+		len = ft_strlen(s);
 	subs = malloc(len + 1);
-	if (subs == NULL)
-		return (NULL);
-	while (i < len && start < ft_strlen(s))
-	{
-		subs[i] = s[start];
-		i++;
-		start++;
-	}
-	subs[i] = '\0';
+	if (!subs)
+		return (0);
+	ft_strlcpy(subs, s + start, len + 1);
 	return (subs);
 }
+
+/*int main()
+{
+	char *subs = ft_substr("Ola k ase", 4, 200);
+	printf("%s\n", subs);
+	return (0);
+}*/
