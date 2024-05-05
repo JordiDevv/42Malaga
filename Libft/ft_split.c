33fb5ff5221fb8930793e592050297561a9ec113
JordiDevv <157953321+JordiDevv@users.noreply.github.com>
2024-05-06 00:57:38 +0200

    Add files via upload

--- Libft/ft_split.c
+++ Libft/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: jsanz-bo <jsanz-bo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/02 18:11:35 by jsanz-bo          #+#    #+#             */
-/*   Updated: 2024/05/02 18:36:20 by jsanz-bo         ###   ########.fr       */
+/*   Updated: 2024/05/06 00:46:07 by jordi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,18 +23,33 @@ static char	*ft_strndup(const char *s, size_t n)
 	return (ptr);
 }
 
-char	**ft_split(char const *s, char c)
+static size_t	ft_strparr(char const *s, char c)
+{
+	size_t	i;
+	size_t	count;
+
+	i = 0;
+	count = 0;
+	while (s[i])
+	{
+		while (s[i] && s[i] == c)
+			i++;
+		if (s[i])
+			count++;
+		while (s[i] && s[i] != c)
+			i++;
+	}
+	return (count);
+}
+
+static char	**ft_do_split(char **a, char const *s, char c)
 {
 	char	**arr;
 	size_t	i;
 	size_t	start;
 	size_t	end;
 
-	if (!s)
-		return (NULL);
-	arr = malloc((ft_strlen(s) + 1) * sizeof(char *));
-	if (!arr)
-		return (NULL);
+	arr = a;
 	i = 0;
 	start = 0;
 	while (s[start])
@@ -47,8 +62,24 @@ char	**ft_split(char const *s, char c)
 			arr[i++] = ft_strndup(s + start, end - start);
 			start = end;
 		}
-		start++;
+		if (s[start])
+			start++;
 	}
 	arr[i] = NULL;
+	return (arr);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**arr;
+	size_t	count;
+
+	if (!s)
+		return (NULL);
+	count = ft_strparr(s, c);
+	arr = malloc((count + 1) * sizeof(char *));
+	if (!arr)
+		return (NULL);
+	ft_do_split(arr, s, c);
 	return (arr);
 }
