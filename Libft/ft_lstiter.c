3177792486337180c591e075176b2afd376789dc
Jorge Sanz Bosch <jsanz-bo@c3r2s4.42malaga.com>
2024-05-13 13:28:55 +0200

    Pulida la lstiter para que utilice un auxiliar en vez de desplazar el puntero original. Terminada lstmap. Añadido el header a las funciones que faltaba. A priori está listo para evaluar (pasa ambos Pacos).

--- Libft/ft_lstiter.c
+++ Libft/ft_lstiter.c
@@ -1,12 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstiter.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jsanz-bo <jsanz-bo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/13 10:41:43 by jsanz-bo          #+#    #+#             */
+/*   Updated: 2024/05/13 11:02:38 by jsanz-bo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 void	ft_lstiter(t_list *lst, void (*f)(void *))
 {
+	t_list	*a_lst;
+
 	if (!lst || !f)
 		return ;
-	while (lst)
+	a_lst = lst;
+	while (a_lst)
 	{
-		f(lst->content);
-		lst = lst->next;
+		f(a_lst->content);
+		a_lst = a_lst->next;
 	}
 }
