f8ab83211cff62aa8404b5cda9e6d3402e314c03
JordiDevv <jordidevv@gmail.com>
2024-06-02 14:04:48 +0200

    Se ha empezado la función principal del printf, y se ha empezado a dividir en funciones para medir el tamaño total de los parámetros y recoger su valor

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -10,6 +10,54 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_printf(char const *, ...)
-{
+#include "libftprintf.h"
 
+static void	ft_get_input(int t_return, char *str, va_list args)
+{
+	str++;
+	if (*str == c)
+	{
+		if (t_return == 1)
+			return (1);
+		return (*str);
+	
+
+static int	ft_size_of_print(char *str, va_list args_s)
+{
+	char 	*start_lo;
+	int	size;
+
+	str_lo = str;
+	size = 0;
+	while (*str_lo)
+	{
+		if (*str_lo == '%')
+			size += ft_get_input(1, str_lo, args_s);
+		size++;
+		str_lo++;
+	}
+	return (size);
+}
+
+int	ft_printf(char const *str, ...)
+{
+	if (!str)
+		return (NULL);
+	char	*p;
+	char	*print;
+	va_list	args;
+	va_list	args_s;
+
+	p = str;
+	va_start(args, p);
+	va_copy(args_s, args);
+	print = malloc(ft_size_of_print(str, args_s));
+	if (!print)
+		return (NULL);
+	while (*p)
+	{
+		if(*p == '%')
+			*p = ft_get_input(0, p, args);
+		p++;
+	}
+	
