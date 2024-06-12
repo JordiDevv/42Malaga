a743ef18f6753b306bad2438b6953d348ed7d099
Jordi <jordidevv@gmail.com>
2024-06-13 01:11:35 +0200

    El header no se había guardado.

--- printf/ft_printf.h
+++ printf/ft_printf.h
@@ -19,6 +19,7 @@
 # include <unistd.h>
 
 size_t	ft_strlen(const char *s);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 int		ft_printf(char const *str, ...);
 int     ft_casthex(int n, int u);
 int	    ft_ptrtohex(void *p);
