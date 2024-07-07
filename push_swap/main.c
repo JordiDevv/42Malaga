448778dc8cdd7953f8f38b24800aec8b92e20584
Jorge Sanz Bosch <jsanz-bo@c3r4s5.42malaga.com>
2024-07-07 15:51:49 +0200

    Primer trasteo con el push_swap; Se está probando a recoger el stack en una función auxiliar, parece que falla. De momento hay que arreglar el Makefile y después seguir con eso.

---
+++ push_swap/main.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jsanz-bo <jsanz-bo@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/07 13:36:21 by jsanz-bo          #+#    #+#             */
+/*   Updated: 2024/07/07 15:43:13 by jsanz-bo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+#include "libft.h"
+#include <stdio.h>
+
+void static	get_stack_a(t_stack *stack_a, int argc, char *argv[])
+{
+	t_stack	tmp;
+	int		prueba;
+	
+	stack_a->next = NULL;
+    while (argc > 1)
+	{
+		stack_a->value = ft_atoi(argv[argc--]);
+		prueba = stack_a->value;
+		tmp.next = stack_a;
+		*stack_a = tmp;
+		printf("%d", prueba);
+	}
+}
+
+int	main(int argc, char *argv[])
+{
+    static t_stack	stack_a;
+	
+	get_stack_a(&stack_a, argc, argv);
+	return (0);
+}
---
+++ push_swap/push_swap.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jsanz-bo <jsanz-bo@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/07 13:58:22 by jsanz-bo          #+#    #+#             */
+/*   Updated: 2024/07/07 14:17:39 by jsanz-bo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+typedef struct s_stack
+{
+	int		        value;
+	struct s_stack  *next;
+}			        t_stack;
+
+#endif
