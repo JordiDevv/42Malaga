e9f3c37a10cc333a63f5547b541f341be7cc0454
Jorge Sanz Bosch <jsanz-bo@c3r10s4.42malaga.com>
2024-07-09 19:16:56 +0200

    Se ha creado un nuevo scripts para las funciones que manejen los nodos; Se ha modificado la lógica de la función get_stack, pero hay que seguir puliéndola para poder enlazar correctamente la lista; Se ha definido una función para crear nodos en nodes.c y se ha declarado en el push_swap.h

--- push_swap/nodes.c
+++ push_swap/nodes.c
@@ -1,36 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   nodes.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jsanz-bo <jsanz-bo@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/07 13:36:21 by jsanz-bo          #+#    #+#             */
-/*   Updated: 2024/07/09 19:12:15 by jsanz-bo         ###   ########.fr       */
+/*   Created: 2024/07/09 18:40:15 by jsanz-bo          #+#    #+#             */
+/*   Updated: 2024/07/09 18:45:13 by jsanz-bo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-#include "libft.h"
-#include <stdio.h>
 
-void static	get_stack_a(t_stack *stack_a, int argc, char *argv[])
+t_stack	*ft_new_node(int value, int index)
 {
-	t_stack	*node;
-	t_stack	*next_node;
-	int		i;
-	
-	i = 1;
-	node = ft_new_node(ft_atoi(argv[i]), i);
-    while (i++ < argc - 1)
-	{
-		next_node = ft_new_node(ft_atoi(argv[i]), i);
-		node->next = next_node;
-	}
-}
+	t_stack	*new_node;
 
-int	main(int argc, char *argv[])
-{
-	get_stack_a(&stack_a, argc, argv);
-	return (0);
+	new_node = malloc(sizeof(t_stack));
+	if (!new_node)
+		return (NULL);
+	new_node->value = value;
+	new_node->index = index;
+	new_node->next = NULL;
+	return (new_node);
 }
--- push_swap/push_swap.h
+++ push_swap/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: jsanz-bo <jsanz-bo@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/07 13:58:22 by jsanz-bo          #+#    #+#             */
-/*   Updated: 2024/07/07 14:17:39 by jsanz-bo         ###   ########.fr       */
+/*   Updated: 2024/07/09 18:45:53 by jsanz-bo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,9 @@
 typedef struct s_stack
 {
 	int		        value;
+	int				index;
 	struct s_stack  *next;
 }			        t_stack;
+t_stack	*ft_new_node(int value, int index);
 
 #endif
