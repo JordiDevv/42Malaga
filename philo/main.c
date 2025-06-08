0508181d9e6db56043cd5abc6b7e9fc524af2e1e
JordiDevv <jordidevv@gmail.com>
2025-06-09 00:14:06 +0200

    Se cambia el nombre del directorio por philo (por exigencia del subject) y se ha creado un función provisional para parsear los argumentos (debemos comprobar que es el comportamiento esperado). También se ha agregado un script utils que de momento alberga un ft_atoi.

--- philo/main.c
+++ philo/main.c
@@ -6,18 +6,50 @@
 /*   By: jsanz-bo <jsanz-bo@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/08 16:55:28 by jsanz-bo          #+#    #+#             */
-/*   Updated: 2025/06/08 17:11:32 by jsanz-bo         ###   ########.fr       */
+/*   Updated: 2025/06/09 00:10:11 by jsanz-bo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
+static int	parse_args(char **args, t_conditions *conditions)
+{
+	int	i;
+
+	i = 1;
+	while (args[i + 1])
+	{
+		if (!args[i][0])
+		{
+			printf(R "Some conditions are missing\n" RE);
+			return (1);
+		}
+		i++;
+	}
+	conditions->n_philo = ft_atoi(args[1]);
+	conditions->t_die = ft_atoi(args[2]);
+	conditions->t_eat = ft_atoi(args[3]);
+	conditions->t_sleep = ft_atoi(args[4]);
+	if (args[5] && args[5][0])
+		conditions->n_eats = ft_atoi(args[5]);
+	else
+		conditions->n_eats = -1;
+	return (0);
+}
+
 int	main(int argc, char *argv[])
 {
+	t_conditions	conditions;
+
 	if (argc < 5 || argc > 6)
 	{
 		printf(R "Invalid number of arguments\n" RE);
 		return (EXIT_ERROR);
+	}
+	else
+	{
+		if (parse_args(argv, &conditions))
+			return (EXIT_ERROR);
 	}
 	return (EXIT_SUCCES);
 }
