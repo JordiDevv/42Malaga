6b318376a5772f850057f9a58c92bc98547317f9
JordiDevv <jordidevv@gmail.com>
2025-06-08 17:12:42 +0200

    Se ha comenzado el header y el main. De momento solo comprueba los argumentos. Se han añadido algunas macros para colores al printear y códigos de salida.

---
+++ philosophers/main.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jsanz-bo <jsanz-bo@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/08 16:55:28 by jsanz-bo          #+#    #+#             */
+/*   Updated: 2025/06/08 17:11:32 by jsanz-bo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philosophers.h"
+
+int	main(int argc, char *argv[])
+{
+	if (argc < 5 || argc > 6)
+	{
+		printf(R "Invalid number of arguments\n" RE);
+		return (EXIT_ERROR);
+	}
+	return (EXIT_SUCCES);
+}
---
+++ philosophers/philosophers.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jsanz-bo <jsanz-bo@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/08 17:04:30 by jsanz-bo          #+#    #+#             */
+/*   Updated: 2025/06/08 17:09:59 by jsanz-bo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+# define G "\033[32m"
+# define R "\033[31m"
+# define Y "\033[33m"
+# define RE "\033[0m"
+
+# define EXIT_ERROR 1
+# define EXIT_SUCCES 0
+
+# include <stdio.h>
+
+#endif
