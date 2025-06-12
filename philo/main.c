4c310180dc46d7ec85172f1bf02e8c2b65447f13
JordiDevv <jordidevv@gmail.com>
2025-06-12 19:13:25 +0200

    Se ha cambiado el tipo de someone_dead por un booleano. Se deja pendiente una función destroy_mutex para el script close. Se empieza el flujo de inicialización de la mesa: Se inicializa el mutex para el print y se recoge el tv. Hay que parsear el tv en start_time y seguir con la inicialización de los filósofos, que se estaba empezando a plantear también.

--- philo/main.c
+++ philo/main.c
@@ -6,18 +6,54 @@
 /*   By: jsanz-bo <jsanz-bo@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/08 16:55:28 by jsanz-bo          #+#    #+#             */
-/*   Updated: 2025/06/11 00:56:20 by jsanz-bo         ###   ########.fr       */
+/*   Updated: 2025/06/12 19:05:38 by jsanz-bo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
+void	philo_life(void *arg)
+{
+
+}
+
+void	init_philos(t_table *table)
+{
+	int	i;
+
+	i = 0;
+	while(i < table->conditions.n_philo)
+	{
+		table->philos[i].id = pthread_create(&table->philos[i].thread,
+				NULL, philo_life, &table->philos[i]);
+		
+	}
+}
+
+static int	init_table(t_table *table)
+{
+	if (pthread_mutex_init(&table->print_mutex, NULL))
+	{
+		//destroy_mutex(table, MSSG);
+		return (EXIT_ERROR);
+	}
+	if (gettimeofday(&table->tv, NULL))
+	{
+		//destroy_mutex(table, MSSG);
+		return (EXIT_ERROR);
+	}
+	table->start_time = //Aquí el tiempo parseado
+	init_philos(table);
+}
+
 int	main(int argc, char *argv[])
 {
-	t_conditions	conditions;
+	t_table	table;
 	
-	if (parse_args(argc, argv, &conditions))
-			return (EXIT_ERROR);
+	if (parse_args(argc, argv, &table.conditions))
+		return (EXIT_ERROR);
+	if (init_table(&table))
+		return (EXIT_ERROR);
 	//Hay que inicializar las estructuras y el flujo, sincronizando el tiempo de los hilos con
 	//un start_time global, que está en table. A partir de ahí habrá que controlar que no todos
 	//los filósofos quieran coger los tenedores al mismo tiempo de primeras, por lo que he visto
--- philo/philosophers.h
+++ philo/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: jsanz-bo <jsanz-bo@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/08 17:04:30 by jsanz-bo          #+#    #+#             */
-/*   Updated: 2025/06/11 00:51:01 by jsanz-bo         ###   ########.fr       */
+/*   Updated: 2025/06/12 19:07:05 by jsanz-bo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,7 @@
 # include <limits.h>
 # include <pthread.h>
 # include <sys/time.h>
+# include <stdbool.h>
 
 typedef struct s_conditions
 {
@@ -53,17 +54,20 @@ typedef struct s_table
 	pthread_mutex_t	*forks;
 	t_philo			*philos;
 	pthread_mutex_t	print_mutex;
+	struct	timeval	tv;
     long			start_time;
-    int				someone_died;
+    bool			someone_died;
     pthread_mutex_t	death_mutex;
 }   t_table;
 
-int	ft_atoi(const char *str);
-int	ft_isdigit(char *arg);
+int		ft_atoi(const char *str);
+int		ft_isdigit(char *arg);
 
 size_t	ft_strlen(const char *s);
-int	ft_strncmp(const char *s1, const char *s2, size_t n);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
 
-int	parse_args(int argc, char **args, t_conditions *conditions);
+int		parse_args(int argc, char **args, t_conditions *conditions);
+
+void	destroy_mutex(t_table *table, char *error_mssg);
 
 #endif
