4db003f18e207d234206a2877d99c13adcfd118d
JordiDevv <jordidevv@gmail.com>
2025-10-05 14:25:53 +0200

    Se ha empezado a crear el flujo del main con un bucle infinito que espera el input del usuario. Se ha declarado una variable string para almacenar ese input.

--- cpp/mod_00/ex01/main.cpp
+++ cpp/mod_00/ex01/main.cpp
@@ -6,6 +6,20 @@
 int main()
 {
     PhoneBook phonebook;
+    std::string cmd;
+
     phonebook.resetIndex();
     std::cout.write(WELCOME_MSG, WELCOME_MSG_L);
+
+    while (1)
+    {
+        std::getline(std::cin, cmd);
+        if (cmd == "ADD")
+        {
+            std::cout.write("Funciona", 9);
+            break;
+        }
+    }
+
+    return (0);
 }
