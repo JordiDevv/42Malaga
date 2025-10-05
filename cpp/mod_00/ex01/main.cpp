3e2d01dd4d2af05a16781ef4dc2f521278ddd476
JordiDevv <jordidevv@gmail.com>
2025-10-05 17:57:14 +0200

    Se actualiza el Makefile. Empezamos el flujo de ft_add e implementamos la lógica de mensajes.

--- cpp/mod_00/ex01/main.cpp
+++ cpp/mod_00/ex01/main.cpp
@@ -14,13 +14,13 @@ int main()
 
     while (1)
     {
+        std::cout.write(CHS_MSG, CHS_MSG_L);
         std::getline(std::cin, input);
 
         if (input == "ADD")
             ft_add(phonebook);
         else if (input == "SEARCH") std::cout.write("Funciona\n", 10);
         else if (input == "EXIT") break;
-        else std::cout.write(WELCOME_MSG, WELCOME_MSG_L);
     }
 
     return (0);
--- cpp/mod_00/ex01/utils.cpp
+++ cpp/mod_00/ex01/utils.cpp
@@ -1,4 +1,19 @@
-void ft_add()
+#include "Contact.hpp"
+#include "PhoneBook.hpp"
+#include "msgs.h"
+#include <iostream>
+
+void ft_add(PhoneBook &phonebook)
 {
-    
+    std::string input;
+    int i = phonebook.getCurrentIndex();
+
+    while (1)
+    {
+        std::cout.write(FN_MSG, FN_MSG_L);
+        std::getline(std::cin, input);
+        if (input != "")
+            break;
+    }
+    std::cout.write(ADDED_MSG, ADDED_MSG_L);
 }
