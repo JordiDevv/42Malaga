05bc806e7edf8fd50ce37f26a309a2d3cdb09523
JordiDevv <jordidevv@gmail.com>
2025-10-10 21:38:22 +0200

    Se implementa la función randomChump, que crea un Zombie en el scope de la propia función y lo hace anunciarse, y se añade en el flujo del main.

--- cpp/mod_01/ex00/headers/Zombie.h
+++ cpp/mod_01/ex00/headers/Zombie.h
@@ -4,5 +4,6 @@
 #include "Zombie.hpp"
 
 Zombie* newZombie(std::string name);
+void randomChump(std::string name);
 
 #endif
--- cpp/mod_01/ex00/main.cpp
+++ cpp/mod_01/ex00/main.cpp
@@ -7,6 +7,8 @@ int main()
     std::cout << "Wake up and live, my son!" << std::endl;
     Zombie* zombie = newZombie("Alberto");
     zombie->announce();
+    std::cout << "WTF! Who are you?" << std::endl;
+    randomChump("Random Zombie");
     delete zombie;
     return (0);
 }
