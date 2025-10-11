e3ea87ad1b895ce4baaa4a1a4c58f13a8bc7d0c0
JordiDevv <jordidevv@gmail.com>
2025-10-11 19:32:44 +0200

    Se ha añadido un constructor por defecto para Zombie, para poder declarar un array dinámico de zombies en zombieHorde. Ahora tenemos que hacer un setter e inicializarlos dentros de esa función.

--- cpp/mod_01/ex01/classes/Zombie.cpp
+++ cpp/mod_01/ex01/classes/Zombie.cpp
@@ -1,7 +1,8 @@
 #include "Zombie.hpp"
 #include <iostream>
 
-Zombie::Zombie(std::string name) { this->name = name; };
-Zombie::~Zombie() { std::cout << name << " has been purified." << std::endl; };
+Zombie::Zombie() {}
+Zombie::Zombie(std::string name) { this->name = name; }
+Zombie::~Zombie() { std::cout << name << " has been purified." << std::endl; }
 
 void Zombie::announce(void) { std::cout << name << ": BraiiiiiiinnnzzzZ..." << std::endl; }
--- cpp/mod_01/ex01/utils/zombieHorde.cpp
+++ cpp/mod_01/ex01/utils/zombieHorde.cpp
@@ -1,6 +1,8 @@
 #include "Zombie.hpp"
+#include <iostream>
 
 Zombie* zombieHorde(int N, std::string name)
 {
-
+    Zombie* zombies = new Zombie[N];
+    return (zombies);
 }
