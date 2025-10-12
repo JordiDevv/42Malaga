f6dbc5f702d85706daa696ede283f12a2de42e96
JordiDevv <jordidevv@gmail.com>
2025-10-12 12:20:43 +0200

    Se han creado las definiciones para HumanA y HumanB (falta algún método) y se ha modificado el constructor de Weapon para que reciba el tipo como argumento. Se ha hecho el main tal y como lo pide el subject.

--- cpp/mod_01/ex03/classes/imps/Weapon.cpp
+++ cpp/mod_01/ex03/classes/imps/Weapon.cpp
@@ -1,5 +1,7 @@
 #include "Weapon.hpp"
+#include "string"
 
-Weapon::Weapon() {}
+Weapon::Weapon(std::string type) { this->type = type; }
+
 const std::string& Weapon::getType() const { return (type); }
 void Weapon::setType(const std::string& type) { this->type = type; }
