896f41630c37c4f793e9f48a0cecef252448b9f8
JordiDevv <jordidevv@gmail.com>
2025-10-15 13:21:12 +0200

    Se agrega un archivo de prueba y se implementan en el main las variables para almacenar los argumentos, la apertura del file, su comprobación y su cierre.

--- cpp/mod_01/ex04/main.cpp
+++ cpp/mod_01/ex04/main.cpp
@@ -1,9 +1,25 @@
 #include "msgs.h"
 #include <iostream>
+#include <fstream>
 
 int main(int argc, char **argv)
 {
-    argv = argv;
     if (argc != 4)
+    {
         std::cout << RED << "The number of arguments is not correct." << RESET << std::endl;
+        return (1);
+    }
+    
+    std::string filename = argv[1];
+    std::fstream file(filename.c_str(), std::ios::in | std::ios::out);
+    if (!file)
+    {
+        std::cout << RED << "Error opening the file." << RESET << std::endl;
+        return (1);
+    }
+
+    std::string s1 = argv[2];
+    std::string s2 = argv[3];
+
+    file.close();
 }
