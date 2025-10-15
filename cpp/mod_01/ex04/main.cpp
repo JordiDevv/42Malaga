78a72b3d48f46ad317da0dec31e86d4c8fc52abd
JordiDevv <jordidevv@gmail.com>
2025-10-15 23:25:51 +0200

    No almacenamos el contenido directamente en un string. Creamos el nuevo archivo con el formato exigido y llamamos a replace_text. En esta función hemos implementado un algoritmo que va leyendo línea por línea el archivo que le pasamos, buscando coincidencias de s1 y construyendo con substr una nueva línea, que es la que copiamos en el archivo de destino. Al final del programa cerramos los archivos.

--- cpp/mod_01/ex04/main.cpp
+++ cpp/mod_01/ex04/main.cpp
@@ -2,6 +2,32 @@
 #include <iostream>
 #include <fstream>
 
+void replace_text(std::ifstream& file, std::ofstream& new_file, const std::string& s1, const std::string& s2)
+{
+    std::string line;
+    while (std::getline(file, line))
+    {
+        std::string new_line;
+        size_t pos = 0;
+
+        while (true)
+        {
+            size_t found = line.find(s1, pos);
+            if (found == std::string::npos)
+            {
+                new_line += line.substr(pos);
+                break;
+            }
+
+            new_line += line.substr(pos, found - pos);
+            new_line += s2;
+            pos = found + s1.size();
+        }
+
+        new_file << new_line << '\n';
+    }
+}
+
 int main(int argc, char **argv)
 {
     if (argc != 4)
@@ -17,10 +43,17 @@ int main(int argc, char **argv)
         std::cout << RED << "Error opening the file." << RESET << std::endl;
         return (1);
     }
+    std::string new_name = filename + ".replace";
+    std::ofstream new_file(new_name.c_str());
+    if (!new_file)
+    {
+        std::cout << RED << "Error creating the file." << RESET << std::endl;
+        return (1);
+    }
 
-    std::string text((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
     std::string s1 = argv[2];
     std::string s2 = argv[3];
 
+    replace_text(file, new_file, s1, s2);
     file.close();
 }
