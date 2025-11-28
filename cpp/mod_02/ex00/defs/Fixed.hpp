382cb5d1385c482ac7b311c0c4f8fd202481cafd
JordiDevv <jordidevv@gmail.com>
2025-11-28 20:55:35 +0100

    Se actualiza el Makefile y se incluye Fixed.hpp en un subdiectorio defs. Se quita también un header innecesario de su definición y se corrige el nombre del main.

--- cpp/mod_02/ex00/defs/Fixed.hpp
+++ cpp/mod_02/ex00/defs/Fixed.hpp
@@ -1,8 +1,6 @@
 #ifndef FIXED_HPP
 #define FIXED_HPP
 
-#include <string>
-
 class Fixed
 {
     private:
@@ -13,7 +11,7 @@ class Fixed
         Fixed(const Fixed& fixed);
         Fixed& operator=(const Fixed& fixed);
         ~Fixed();
-        
+
         int getRawBits(void) const;
         void setRawBits(int const raw);
 };
