215554c1b8f34861eb13423a85d6b0960765c942
JordiDevv <jordidevv@gmail.com>
2025-12-03 19:57:56 +0100

    Se ajusta el main al del ejercicio actual y se quitan todos los mensajes de control en las implementaciones de Fixed.

--- cpp/mod_02/ex02/Fixed.cpp
+++ cpp/mod_02/ex02/Fixed.cpp
@@ -3,47 +3,24 @@
 #include <cmath>
 
 //Canonical implementations
-Fixed::Fixed() : value(0)
-{ std::cout << "Default constructor called" << std::endl; }
-
-Fixed::Fixed(const Fixed& fixed) : value(fixed.value)
-{ std::cout << "Copy constructor called" << std::endl; }
+Fixed::Fixed() : value(0) {}
+Fixed::Fixed(const Fixed& fixed) : value(fixed.value) {}
 
 Fixed& Fixed::operator=(const Fixed& fixed)
 {
-    std::cout << "Copy assignment operator called" << std::endl;
     if (this != &fixed) this->value = fixed.value;
     return *this;
 }
 
-Fixed::~Fixed()
-{ std::cout << "Destructor called" << std::endl; }
+Fixed::~Fixed() {}
 
 //Constructors for parameterized values
-Fixed::Fixed(const int n) 
-{
-    std::cout << "Int constructor called" << std::endl;
-    value = n << fracBits;
-}
-
-Fixed::Fixed(const float n)
-{
-    std::cout << "Float constructor called" << std::endl;
-    value = roundf(n * (1 << fracBits));
-}
+Fixed::Fixed(const int n) { value = n << fracBits; }
+Fixed::Fixed(const float n) { value = roundf(n * (1 << fracBits)); }
 
 //Getters and setters
-int Fixed::getRawBits() const
-{
-    std::cout << "getRawBits member function called" << std::endl;
-    return value;
-}
-
-void Fixed::setRawBits(const int raw)
-{
-    std::cout << "setRawBits member function called" << std::endl;
-    value = raw;
-}
+int Fixed::getRawBits() const { return value; }
+void Fixed::setRawBits(const int raw) { value = raw; }
 
 //Getters with parsed values
 float Fixed::toFloat() const { return (float)value / (1 << fracBits); }
@@ -96,16 +73,10 @@ Fixed Fixed::operator/(const Fixed& divisor) const
 //Overload for increment and decrement operators
 
 Fixed& Fixed::operator++()
-{
-    value += 1;
-    return *this;
-}
+{ value += 1; return *this; }
 
 Fixed& Fixed::operator--()
-{
-    value -= 1;
-    return *this;
-}
+{ value -= 1; return *this; }
 
 Fixed Fixed::operator++(int)
 {
@@ -124,6 +95,9 @@ Fixed Fixed::operator--(int)
 //Overloaded static function members for minimum and maximum comparatives
 
 Fixed& Fixed::min(Fixed& a, Fixed& b) { return a < b ? a : b; }
-const Fixed& Fixed::min(const Fixed& a, const Fixed& b) { return a < b ? a : b; }
+const Fixed& Fixed::min(const Fixed& a, const Fixed& b)
+{ return a < b ? a : b; }
+
 Fixed& Fixed::max(Fixed& a, Fixed& b) { return a > b ? a : b; }
-const Fixed& Fixed::max(const Fixed& a, const Fixed& b) { return a > b ? a : b; }
+const Fixed& Fixed::max(const Fixed& a, const Fixed& b)
+{ return a > b ? a : b; }
