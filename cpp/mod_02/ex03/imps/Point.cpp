36f2121c2eb48f431230d6c8c4075caef88efa5c
JordiDevv <jordidevv@gmail.com>
2025-12-04 14:47:34 +0100

    Se implementan los métodos iniciales para Point. Tras mucho pelear buscando la solución para el overload de asignación descubrimos el patrón placement new, que no reserva memoria, si no que reempleza la del objeto en cuestión, y sirve para clases triviales (sin manejo especial de recursos ni memoria) en casos como este, donde sus parámetros son const y no se les puede asignar un valor directamente.

---
+++ cpp/mod_02/ex03/imps/Point.cpp
@@ -0,0 +1,24 @@
+#include "Point.hpp"
+#include "Fixed.hpp"
+
+   // **************************************************** //
+  //              Cannonical implementations              //
+ // **************************************************** //
+
+Point::Point() : x(0), y(0) {}
+Point::Point(const Point& point) : x(point.x), y(point.y) {}
+
+Point& Point::operator=(const Point& point)
+{
+    if (this != &point) new (this) Point(point);
+    return *this;
+}
+
+Point::~Point() {}
+
+
+   // **************************************************** //
+  //        Constructors for parameterized values         //
+ // **************************************************** //
+
+Point::Point(const float x, const float y) : x(x), y(y) {}
