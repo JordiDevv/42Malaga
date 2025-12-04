fa9b3878da9b9e1b2950d28270f061023817c965
JordiDevv <jordidevv@gmail.com>
2025-12-04 21:53:11 +0100

    Adaptamos una fórmula matemática a nuestro código que es la que comprueba si un punto está dentro de los vértices de un triángulo y se deja el main con una prueba. El ejercicio debería estar listo ya.

--- cpp/mod_02/ex03/bsp.cpp
+++ cpp/mod_02/ex03/bsp.cpp
@@ -1,10 +1,21 @@
 #include "Point.hpp"
 
+static Fixed sign(const Point& p, const Point& a, const Point& b)
+{ return (p.getX() - b.getX()) * (a.getY() - b.getY()) - (a.getX() - b.getX()) * (p.getY() - b.getY()); }
+
 bool bsp(Point const a, Point const b, Point const c, Point const point)
 {
     if ((point.getX() == a.getX() && point.getY() == a.getY()) ||
         (point.getX() == b.getX() && point.getY() == b.getY()) ||
         (point.getX() == c.getX() && point.getY() == c.getY()))
             return false;
-    return true;
+
+    Fixed d1 = sign(point, a, b);
+    Fixed d2 = sign(point, b, c);
+    Fixed d3 = sign(point, c, a);
+
+    bool hasNeg = (d1 < Fixed(0)) || (d2 < Fixed(0)) || (d3 < Fixed(0));
+    bool hasPos = (d1 > Fixed(0)) || (d2 > Fixed(0)) || (d3 > Fixed(0));
+
+    return !(hasNeg && hasPos);
 }
--- cpp/mod_02/ex03/main.cpp
+++ cpp/mod_02/ex03/main.cpp
@@ -2,13 +2,22 @@
 #include "Point.hpp"
 #include <iostream>
 
+#define GREEN "\033[1;32m"
+#define BLUE "\033[1;36m"
+#define RESET "\033[0m"
+
 int main()
 {
     Point a(1.0f, 2.0f);
     Point b(3.0f, 4.0f);
     Point c(5.0f, 2.0f);
 
-    Point point(1.1f, 2.0f);
+    Point point(1.1f, 1.9f);
 
-    return bsp(a, b, c, point);
+    if (bsp(a, b, c, point))
+        std::cout << GREEN << "THE POINT IS INSIDE THE TRIANGLE." << RESET << std::endl;
+    else
+        std::cout << BLUE << "THE POINT IS OUTSIDE THE TRIANGLE." << RESET << std::endl;
+
+    return 0;
 }
