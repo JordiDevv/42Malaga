6bd908544f6eba00b645812967417a1dcd04d7de
JordiDevv <jordidevv@gmail.com>
2025-12-12 10:17:12 +0100

    Se prototipa la definición de la clase ClapTrap y se añaden los archivos fuente al Makefile.

---
+++ cpp/mod_03/ex00/defs/ClapTrap.hpp
@@ -0,0 +1,25 @@
+#ifndef CLAPTRAP_H
+#define CLAPTRAP_H
+
+#include <string>
+
+class ClapTrap
+{
+    private:
+        std::string name;
+        int health = 10;
+        int energy = 10;
+        int attackDamage = 0;
+    
+    public:
+        ClapTrap();
+        ClapTrap(const ClapTrap& ref);
+        ClapTrap& operator=(const ClapTrap& ref);
+        ~ClapTrap();
+
+        void attack(const std::string& target);
+        void takeDamage(unsigned int amount);
+        void beRepaired(unsigned int amount);
+};
+
+#endif
