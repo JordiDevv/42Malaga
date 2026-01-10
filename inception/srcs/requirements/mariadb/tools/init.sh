950df0a53582a27676c0b590d0cee9201348599c
JordiDevv <jordidevv@gmail.com>
2026-01-10 20:38:43 +0100

    Hemos agregado una regla delete al Makefile para poder borrar rápidamente el data que construimos en el host para los volúmenes persistentes. Hemos juntado todas las instrucciones mysql del init de mariadb en un heredoc, hemos añadido un wait para el proceso en segundo plano de arranque del servidor antes de llevarlo al PID 1, y hemos añadido IDENTIFIED WITH mysql_native_password, para solucionar un problema de acceso sin contraseña del root al arrancar el proceso.

--- inception/srcs/requirements/mariadb/tools/init.sh
+++ inception/srcs/requirements/mariadb/tools/init.sh
@@ -10,15 +10,16 @@ until mysqladmin ping >/dev/null 2>&1; do
     sleep 1
 done
 
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$ROOT_PASS'; FLUSH PRIVILEGES;"
-
-mysql -e "CREATE DATABASE IF NOT EXISTS $WP_DB;"
-
-mysql -e "CREATE USER IF NOT EXISTS '$WP_DB_USER'@'%' IDENTIFIED BY '$DB_USER_PASS';"
-mysql -e "GRANT ALL PRIVILEGES ON $WP_DB.* TO '$WP_DB_USER'@'%';"
-
-mysql -e "FLUSH PRIVILEGES;"
+mysql <<EOF 
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '$ROOT_PASS'; FLUSH PRIVILEGES;
+CREATE DATABASE IF NOT EXISTS $WP_DB;
+CREATE USER IF NOT EXISTS '$WP_DB_USER'@'%' IDENTIFIED BY '$DB_USER_PASS';
+GRANT ALL PRIVILEGES ON $WP_DB.* TO '$WP_DB_USER'@'%';
+FLUSH PRIVILEGES;
+EOF
 
 mysqladmin shutdown
+
+wait "$pid"
 
 exec mysqld_safe
