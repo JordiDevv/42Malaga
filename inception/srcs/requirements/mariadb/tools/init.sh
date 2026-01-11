1148d0e641a44f32eb5f3fc87e4f488d3f1209ac
JordiDevv <jordidevv@gmail.com>
2026-01-11 03:36:22 +0100

    Hemos llevado el init para mariadb al next level. Teníamos algunos problemas de configuración y permisos a la hora de arrancar el server. Ahora, aseguramos el ownership de mysql, inicializamos correctamente la db, y lo hacemos con mariadb directamente, en vez de mysql, comprobamos la creación del socket para asegurar el arranque del servidor, inyectamos todas las instrucciones sql mediante un heredoc que corre sin autentificación por password (como lo requiere el arranque inicial por unix socket de mariadb), apagamos el servidor temporal, ahora ya usando la contraseña configurada desde el archivo de secrets para el root, y esperamos a que finalice el proceso. Finalmente arrancamos el server en el PID 1, como ya hacíamos, pero ahora con el binario de mariadb, no con el wrapper mysqld_safe. Mañana lo testearemos, pero tiene pinta que debería funcionar correctamente, y desde luego que hemos avanzado bastante, sobre todo en la comprensión de cómo funciona.

--- inception/srcs/requirements/mariadb/tools/init.sh
+++ inception/srcs/requirements/mariadb/tools/init.sh
@@ -1,25 +1,57 @@
 #!/bin/bash
 set -e
 
+# We ensure ownership for the mysql user and the temporary directory for mysql daemon
+chown -R mysql:mysql /var/lib/mysql
+mkdir -p /run/mysqld
+chown -R mysql:mysql /run/mysqld
+
+# Some vars for the content in secrets
 ROOT_PASS=$(cat $WP_DB_ROOT_PASS)
 DB_USER_PASS=$(cat $WP_DB_USER_PASS)
 
-mysqld_safe --skip-networking & pid="$!"
+# We check if MariaDB was initialized before
+if [ ! -d "/var/lib/mysql/mysql" ]; then
 
-until mysqladmin ping >/dev/null 2>&1; do
-    sleep 1
-done
+# ------------- INITIALIZE THE TEMPORARY SERVER AND WAIT FOR THE SOCKET --------------------
 
-mysql <<EOF 
-ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '$ROOT_PASS'; FLUSH PRIVILEGES;
-CREATE DATABASE IF NOT EXISTS $WP_DB;
-CREATE USER IF NOT EXISTS '$WP_DB_USER'@'%' IDENTIFIED BY '$DB_USER_PASS';
-GRANT ALL PRIVILEGES ON $WP_DB.* TO '$WP_DB_USER'@'%';
-FLUSH PRIVILEGES;
-EOF
+    mariadb-install-db --user=mysql --datadir=/var/lib/mysql
 
-mysqladmin shutdown
+    /usr/sbin/mariadbd --user=mysql --datadir=/var/lib/mysql --skip-networking &
+    pid="$!"
 
-wait "$pid"
+    echo -e "Initializing MariaDB server with PID $pid...\n"
+    i=0
+    while ! [ -S /run/mysqld/mysqld.sock ]; do
+        sleep 1
+        ((i++))
+        if [ "$i" -gt 30 ]; then
+            echo "Error initializing the MariaDB server. Aborting...\n"
+            exit 1
+        fi
+    done
 
-exec mysqld_safe
+# -------------------------------------------------------------------------------------------
+
+# -------------- CREATE DB, CONFIGURE USERS AND SHUT DOWN THE TEMPORARY SERVER --------------
+
+    echo -e "Running initial configuration for the wp db...\n"
+    mariadb -u root <<-EOF
+        ALTER USER 'root'@'localhost' IDENTIFIED BY '${ROOT_PASS}';
+        CREATE DATABASE IF NOT EXISTS ${WP_DB};
+        CREATE USER IF NOT EXISTS '${WP_DB_USER}'@'%' IDENTIFIED BY '${DB_USER_PASS}';
+        GRANT ALL PRIVILEGES ON ${WP_DB}.* TO '${WP_DB_USER}'@'%';
+	EOF
+
+    echo -e "Shutting down the temporary server...\n"
+    mariadb-admin -u root -p"${ROOT_PASS}" shutdown
+
+    wait "$pid"
+
+# -------------------------------------------------------------------------------------------
+
+fi
+
+echo -e "Starting MariaDB server in PID 1...\n"
+# Now we execute the server as the main process, in PID 1
+exec /usr/sbin/mariadbd --user=mysql --datadir=/var/lib/mysql --bind-address=0.0.0.0
