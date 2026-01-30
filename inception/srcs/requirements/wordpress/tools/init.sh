2a17d7369abcbe6570f3186da3d457f73d77c24a
JordiDevv <jordidevv@gmail.com>
2026-01-30 19:55:48 +0100

    Se documenta el init de wordpress.

--- inception/srcs/requirements/wordpress/tools/init.sh
+++ inception/srcs/requirements/wordpress/tools/init.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+# If any process returns a value non-zero, the script stops
 set -e
 
+# Read the password hidden in the secret file
 DB_USER_PASS=$(cat $WP_DB_USER_PASS)
 
+# We check if Wordpress has been already configurated
 if [ ! -f /var/www/html/wp-config.php ]; then
+
+# ---------------------- SET THE CONFIG FILE FOR OUR WORDPRESS -----------------------------
+
     echo "Configuring WordPress...\n"
 
     cp /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
@@ -13,6 +19,10 @@ if [ ! -f /var/www/html/wp-config.php ]; then
     sed -i "s/username_here/${WP_DB_USER}/" /var/www/html/wp-config.php
     sed -i "s/password_here/${DB_USER_PASS}/" /var/www/html/wp-config.php
     sed -i "s/localhost/${WP_DB_HOST}/" /var/www/html/wp-config.php
+
+# -------------------------------------------------------------------------------------------
+
 fi
 
+# Run the php interpeter in PID 1
 exec php-fpm8.2 -F
