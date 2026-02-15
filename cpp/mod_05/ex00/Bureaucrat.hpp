10c1b75a7a4aa98cef000a6dc153e67e570e7120
JordiDevv <jordidevv@gmail.com>
2026-02-15 20:12:10 +0100

    Se define la clase Bureaucrat en su hpp, con lo que, interpretamos, son los requerimientos del subject. Incluye dos variables privadas para el nombre y el grado, métodos canónicos ortodoxos, getters, dos métodos para incrementar y decrementar el grado, y dos excepciones para los casos en que el grado esté fuera del rango especificado en el subject. También tiene un overload para el operador de desplazamiento a la izquierda, a fin de enviar la información solicitada por el outstream.ç

---
+++ cpp/mod_05/ex00/Bureaucrat.hpp
@@ -0,0 +1,39 @@
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+#include <string>
+#include <exception>
+
+class Bureaucrat
+{
+    private:
+        const std::string name;
+        int grade;
+
+    public:
+        Bureaucrat();
+        Bureaucrat(const Bureaucrat& ref);
+        Bureaucrat& operator=(const Bureaucrat& ref);
+        ~Bureaucrat();
+
+        const std::string& getName() const;
+        const int& getGrade() const;
+
+        void incGrade();
+        void decGrade();
+
+        class GradeTooHighException : public std::exception
+        {
+            public:
+                const char* what() const throw();
+        };
+        class GradeTooLowException : public std::exception
+        {
+            public:
+                const char* what() const throw();
+        };
+};
+
+std::ostream& operator<<(std::ostream& os, const Bureaucrat& bureaucrat);
+
+#endif
