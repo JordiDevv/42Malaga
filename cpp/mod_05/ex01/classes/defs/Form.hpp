a196386c6c5d5b8e33c8d04eaef6513c12935836
JordiDevv <jordidevv@gmail.com>
2026-02-18 13:20:25 +0100

    Se rellena el hpp del Form con las propiedades pedidas y una primera batería de métodos: Los métodos canónicos y el constructor parametrizado, getters, exception classes para los límites de los grados, y el overload para el operador de desplazamiento.

---
+++ cpp/mod_05/ex01/classes/defs/Form.hpp
@@ -0,0 +1,41 @@
+#ifndef FORM_HPP
+#define FORM_HPP
+
+#include <string>
+
+class Form
+{
+    private:
+        const std::string name;
+        bool isSigned;
+        const int gradeToSign;
+        const int gradeToExec;
+    
+    public:
+        Form();
+        Form(const Form& ref);
+        Form& operator=(const Form& ref);
+        ~Form();
+
+        Form(const std::string& name, const int& grade);
+
+        const std::string& getName() const;
+        const bool& getIfSigned() const;
+        const int& getGradeToSign() const;
+        const int& getGradeToExec() const;
+
+        class GradeTooHighException : public std::exception
+        {
+            public:
+                const char* what() const throw();
+        };
+        class GradeTooLowException : public std::exception
+        {
+            public:
+                const char* what() const throw();
+        };
+};
+
+std::ostream& operator<<(std::ostream& os, const Form& form);
+
+#endif
