5c369cf2e01db75453efe5ada03b4aa5aee3144e
JordiDevv <jordidevv@gmail.com>
2026-03-02 19:40:09 +0100

    Se inicializa el directorio para el módulo 6 con el subdirectorio para el ex00, con su Makefile, la definición de una clase no instanciable ScalarConverter, y el archivo de implementación correspondiente (sin desarrollar).

---
+++ cpp/mod_06/ex00/classes/imps/ScalarConverter.cpp
@@ -0,0 +1,11 @@
+#include "ScalarConverter.hpp"
+#include <string>
+
+  // **************************************************** //
+ //                  Converter method                    //
+// **************************************************** //
+
+	void ScalarConverter::convert(const std::string& literal)
+	{
+
+	}
