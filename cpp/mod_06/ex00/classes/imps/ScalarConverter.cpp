4aa02173f674f1e126a6a58f513836911ab65580
JordiDevv <jordidevv@gmail.com>
2026-03-03 09:56:11 +0100

    Se implementa una pequeña función auxiliar para parsear el tipo de dato e identificarlo. De momento solo funciona para char e int y hemos hecho una pequeña prueba para comprobar que funciona correctamente.

--- cpp/mod_06/ex00/classes/imps/ScalarConverter.cpp
+++ cpp/mod_06/ex00/classes/imps/ScalarConverter.cpp
@@ -1,11 +1,24 @@
 #include "ScalarConverter.hpp"
 #include <string>
+#include <cctype>
+#include <iostream>
 
   // **************************************************** //
  //                  Converter method                    //
 // **************************************************** //
 
+	std::string parser(const std::string& literal)
+	{
+		if (literal.length() == 1)
+		{
+			if (isdigit(literal.c_str()[0])) return "int";
+			return "char";
+		}
+	}
+
 	void ScalarConverter::convert(const std::string& literal)
 	{
-
+		std::string type;
+		type = parser(literal);
+		std::cout << type << std::endl;
 	}
