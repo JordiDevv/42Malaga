11566a0464c4fe6f930f3cb983e6d0d03773e5ee
JordiDevv <jordidevv@gmail.com>
2026-03-14 17:41:46 +0100

    Se añade el uso de size_t para el manejo de tamaño e índices; un destructor; y el uso del operador []; a la definición de la clase Array.

--- cpp/mod_07/ex02/templates/Array.hpp
+++ cpp/mod_07/ex02/templates/Array.hpp
@@ -1,19 +1,25 @@
 #ifndef ARRAY_HPP
 #define ARRAY_HPP
 
+#include <cstdlib>
+
 template <typename T>
 class Array
 {
     private:
         T* element;
+        size_t size;
 
     public:
         Array();
         Array(const Array& ref);
         Array& operator=(const Array& ref);
+        ~Array();
 
         Array(unsigned int n);
-        unsigned int size() const;
+
+        T& operator[](size_t index)
+        size_t size() const;
 };
 
 #include <Array.tpp>
