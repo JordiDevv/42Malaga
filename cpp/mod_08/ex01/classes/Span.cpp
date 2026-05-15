3df09d5d9aa4c8e6baed1fcb924c87e4ac1c0726
JordiDevv <jordidevv@gmail.com>
2026-05-15 12:07:53 +0200

    We implemented the logic for shortestSpan member function.

--- cpp/mod_08/ex01/classes/Span.cpp
+++ cpp/mod_08/ex01/classes/Span.cpp
@@ -37,14 +37,23 @@
     int Span::shortestSpan()
     {
         if (_container.size() < 2) throw NotEnoughNumbersException();
-        return 0;
+
+        int smaller = _container[0] < _container[1] ? _container[0] : _container[1];
+        int bigger = _container[1] > _container[1] ? _container[0] : _container[1];
+        for (size_t i = 2; i < _container.size(); i++)
+        {
+            if (_container[i] < bigger && _container[i] >= smaller) bigger = _container[i];
+            if (_container[i] > smaller && _container[i] <= bigger) smaller = _container[i];
+        }
+
+        return bigger - smaller;
     }
 
     int Span::longestSpan()
     {
         if (_container.size() < 2) throw NotEnoughNumbersException();
         
-        int smallest= _container[0];
+        int smallest = _container[0];
         int biggest = _container[0];
         for (size_t i = 1; i < _container.size(); i++)
         {
