0cc12333b328d17e6ae9a9351a9f371a3b389d8e
JordiDevv <jordidevv@gmail.com>
2026-05-20 11:53:01 +0200

    We added a final check that avoids using any file of the exercise as the output file and included all the validations in a auxiliar function called validArgs.

--- cpp/mod_09/ex00/main.cpp
+++ cpp/mod_09/ex00/main.cpp
@@ -1,18 +1,39 @@
 #include <fstream>
 #include <iostream>
 
-int main(int argc, char** argv)
+bool validArgs(int argc, char** argv)
 {
     if (argc != 2)
     {
         std::cerr << "You need to introduce only the output data file as an argument" << std::endl;
-        return 1;
+        return false;
     }
-    if (std::string(argv[1]) == "data.csv")
+
+    std::string outputFile(argv[1]);
+    if (outputFile == "data.csv")
     {
         std::cerr << "The input data shouldn't be the output file" << std::endl;
-        return 1;
+        return false;
     }
+
+    std::string srcData[] =
+    {"classes/BitcoinExchange.cpp", "classes/BitcoinExchange.hpp", "main.cpp", "Makefile"};
+    size_t srcDataSize = sizeof(srcData) / sizeof(srcData[0]);
+    for (size_t i = 0; i < srcDataSize; i++)
+    {
+        if (outputFile == srcData[i])
+        {
+            std::cerr << "Don't you dare" << std::endl;
+            return false;
+        }
+    }
+
+    return true;
+}
+
+int main(int argc, char** argv)
+{
+    if (!validArgs(argc, argv)) return 1;
     
     std::ofstream outputData(argv[1]);
     std::ifstream data("data.csv");
