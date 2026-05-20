16023a0d46fc4b9db805b22551f212c79df48a69
JordiDevv <jordidevv@gmail.com>
2026-05-20 11:23:18 +0200

    We init the hpp for the BitcoinExchange and prepared the main for reading the input, also adding another error check if the user tries to use the input data as the ouput file.

--- cpp/mod_09/ex00/main.cpp
+++ cpp/mod_09/ex00/main.cpp
@@ -5,12 +5,25 @@ int main(int argc, char** argv)
 {
     if (argc != 2)
     {
-        std::cout << "You need to introduce only the output data file as an argument" << std::endl;
+        std::cerr << "You need to introduce only the output data file as an argument" << std::endl;
+        return 1;
+    }
+    if (std::string(argv[1]) == "data.csv")
+    {
+        std::cerr << "The input data shouldn't be the output file" << std::endl;
         return 1;
     }
     
     std::ofstream outputData(argv[1]);
+    std::ifstream data("data.csv");
+
+    std::string line;
+    while (std::getline(data, line))
+    {
+        // procesar linea
+    }
 
     outputData.close();
+    data.close();
     return 0;
 }
