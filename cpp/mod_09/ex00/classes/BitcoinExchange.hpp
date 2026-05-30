23446b1b485f0522ca70878971d9bb6403d67e6d
JordiDevv <jordidevv@gmail.com>
2026-05-30 22:04:23 +0200

    We corrected some error messages.

--- cpp/mod_09/ex00/classes/BitcoinExchange.hpp
+++ cpp/mod_09/ex00/classes/BitcoinExchange.hpp
@@ -1,9 +1,9 @@
 #ifndef BITCOINEXCHANGE_HPP
 #define BITCOINEXCHANGE_HPP
 
-#define ERR_NARGS "Error: you need to introduce only the output data file as an argument"
+#define ERR_NARGS "Error: you need to introduce only the input file as argument"
 #define ERR_NINPUT "Error: input file couldn't be open"
-#define ERR_INASOUT "Error: the input data shouldn't be the output file"
+#define ERR_INASOUT "Error: the database shouldn't be the input file"
 #define ERR_SRCASOUT "Don't you dare"
 
 #define ERR_BADINPUTLINE "Error: bad input => "
