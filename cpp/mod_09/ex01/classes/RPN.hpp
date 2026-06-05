863af9339d42bfaa9554d922ca1bad9681d4ac4b
JordiDevv <jordidevv@gmail.com>
2026-06-05 21:25:35 +0200

    We removed the old validation from the main and declared a processLine method into RPN

--- cpp/mod_09/ex01/classes/RPN.hpp
+++ cpp/mod_09/ex01/classes/RPN.hpp
@@ -18,6 +18,7 @@ class RPN
         ~RPN();
 
         bool validLine(const std::string& line);
+        void processLine(const std::string& line);
 };
 
 #endif
--- cpp/mod_09/ex01/main.cpp
+++ cpp/mod_09/ex01/main.cpp
@@ -1,44 +1,6 @@
 #include "RPN.hpp"
 #include <iostream>
 
-bool isOperator(char c)
-{
-    switch (c)
-    {
-        case '+':
-        case '-':
-        case '*':
-        case '/':
-            return true;
-        default:
-            return false;
-    }
-}
-
-bool validInput(int argc, char** argv)
-{
-    if (argc != 2) return false;
-
-    size_t n = 0;
-    size_t op = 0;
-    for (size_t i = 0; argv[1][i]; i++)
-    {
-        if (isdigit(argv[1][i])) n++;
-        else if (isOperator(argv[1][i])) op++;
-        else return false;
-
-        if (argv[1][i + 1])
-        {
-            if (argv[1][i + 1] != ' ') return false;
-            else i++;
-        }
-        
-        if (n - op < 1) return false;
-    }
-
-    return n - op == 1 ? true : false;
-}
-
 int main(int argc, char** argv)
 {
     if (argc != 2) return false;
@@ -49,6 +11,8 @@ int main(int argc, char** argv)
         std::cerr << "Error" << std::endl;
         return 1;
     }
+
+    rpn.processLine(argv[1]);
 
     return 0;
 }
