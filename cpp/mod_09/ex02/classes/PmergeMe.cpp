c01fb17c1e0bae794773e6ab60f82c57c64a36a5
JordiDevv <jordidevv@gmail.com>
2026-09-09 03:13:17 +0200

    We keep on our analysis to reach the optimal comparissions number. We now insert the straggle before the minor and it works for seven and eight, but still having more than optimal from 8 and beyond. We added debug for comparissions after every calling in fordJohnson. I don't know what day it is.

--- cpp/mod_09/ex02/classes/PmergeMe.cpp
+++ cpp/mod_09/ex02/classes/PmergeMe.cpp
@@ -165,10 +165,15 @@
     {
         FordJohnsonData<Container> data;
         initData(input, data);
+        std::cout << _comparissions << std::endl; // DEBUG
         data.pairs = sortPairsByMajor<Container>(data.pairs);
+        std::cout << _comparissions << std::endl; // DEBUG
         initMainChain(data);
-        if (data.pairs.size() > 1) jacobsthalInsertion(data);
+        std::cout << _comparissions << std::endl; // DEBUG
         if (data.hasStraggler) binaryInsertion(data.mainChain, data.straggler, data.mainChain.size());
+        std::cout << _comparissions << std::endl; // DEBUG
+        if (data.pairs.size() > 1) jacobsthalInsertion(data);
+        std::cout << _comparissions << std::endl; // DEBUG
         // The example with the time in the subject
         // We're analyzing the whole flow with small inputs to finnish the optimal comparissions
         return data.mainChain;
@@ -243,7 +248,10 @@
         typename PairContainer<Container>::type result =
             sortPairsByMajor<Container>(winners);
 
-        jacobsthalForLosers<Container>(result, losers);
+        if (result.size() > 1)
+            jacobsthalForLosers<Container>(result, losers);
+        else
+            result.insert(result.begin(), losers[0]);
 
         if (hasStraggler)
         {
